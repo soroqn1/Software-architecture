0d145fd5df910e30d18a6d946b50b4edbea3f59a
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-04-30 21:13:35 +0300

    refactor: remove academic domain models and implement initial Animal entity structure

--- src/lab1/main.py
+++ src/lab1/main.py
@@ -1,64 +1,31 @@
-class Teacher:
+from datetime import datetime
+from enum import Enum
+
+
+class Location(Enum):
+    OWNER = "Хазяїн"
+    PET_SHOP = "Зоомагазин"
+    WILD = "Воля"
+
+
+class Animal:
     def __init__(self, name):
         self.name = name
 
+        self.eyes = 2
+        self.paws = 4
+        self.wings = 0
 
-class StudentGroup:
-    def __init__(self, name, size):
-        if size < 13:
-            raise ValueError("Група має бути не менше 13 чол.")
-        self.name = name
-        self.size = size
+        self.location = Location.WILD
+        self.last_meal_time = datetime.now()
+        self.meals_today = 0
+        self.is_alive = True
 
+        self.on_died = []
+        self.on_hungry = []
 
-class Activity:
-    def __init__(self, name, teacher):
-        self.name = name
-        self.teacher = teacher
+    def eat(self):
+        pass
 
-    def conduct(self, group):
-        print(
-            f"[{self.name}] проводиться для групи {group.name}. Викладач: {self.teacher.name}"
-        )
-
-
-class Discipline:
-    def __init__(self, name):
-        self.name = name
-        self.activities = []
-
-    def add_activity(self, activity):
-        self.activities.append(activity)
-
-    def study(self, group):
-        print(f"\n--- Початок вивчення: {self.name} ---")
-        for activity in self.activities:
-            activity.conduct(group)
-
-
-class HigherMath(Discipline):
-    def __init__(self, lecture_teacher, practice_teacher):
-        super().__init__("Вища математика")
-        self.add_activity(Activity("Лекція", lecture_teacher))
-        self.add_activity(Activity("Практична", practice_teacher))
-        self.add_activity(Activity("Курсова робота", practice_teacher))
-        self.add_activity(Activity("МКР", lecture_teacher))
-        self.add_activity(Activity("Екзамен", lecture_teacher))
-
-
-class English(Discipline):
-    def __init__(self, practice_teacher):
-        super().__init__("Англійська")
-        self.add_activity(Activity("Практична", practice_teacher))
-        self.add_activity(Activity("МКР", practice_teacher))
-        self.add_activity(Activity("Залік", practice_teacher))
-
-
-class Physics(Discipline):
-    def __init__(self, lecture_teacher, lab_teacher):
-        super().__init__("Фізика")
-        self.add_activity(Activity("Лекція", lecture_teacher))
-        self.add_activity(Activity("Лабораторна робота", lab_teacher))
-        self.add_activity(Activity("Курсова робота", lecture_teacher))
-        self.add_activity(Activity("МКР", lecture_teacher))
-        self.add_activity(Activity("Екзамен", lecture_teacher))
+    def move(self):
+        pass
